636631d5f88cfdcfb2f51ed754de5089abd18a87
txqtiffany <txqtiffany@gmail.com>
2021-03-18 00:54:47 +0800

    Document my_t.test

--- R/my_t.test.R
+++ R/my_t.test.R
@@ -1,3 +1,25 @@
+#' Student's t-Test function
+#'
+#' This function performs a one sample t-test in R.
+#'
+#' @param x a numeric vector of data.
+#' @param alternative a character string specifying the alternative hypothesis.
+#'   This should only accept \code{"two.sided"}, \code{"less"}, or \code{"greater"}.
+#' @param mu a number indicating the null hypothesis value of the mean.
+#' @keywords inference
+#'
+#' @return a list with elements:
+#'
+#'  \item{test_stat}{the numeric test statistic}
+#'  \item{df}{the degrees of freedom}
+#'  \item{alternative}{the value of the parameter alternative}
+#'  \item{p_val}{the numeric p-value}
+#'
+#' @examples
+#' trial <- rnorm(30, 4, 7)
+#' t.test(trial, alternative = "less", mu = 4)
+#'
+#' @export
 my_t.test <- function(x, alternative, mu) {
   if (alternative != "greater" && alternative != "less" && alternative != "two.sided") {
     stop('The alternative must be either "two.sided", "greater", or "less".')
