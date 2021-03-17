d37ea5646b66d668c45c5f56c50d57723ac2effe
txqtiffany <txqtiffany@gmail.com>
2021-03-18 02:24:29 +0800

    Document my_lm

--- R/my_lm.R
+++ R/my_lm.R
@@ -1,16 +1,31 @@
-# my_lm <- function(formula, data) {
-#   if (class(formula) != "formula") {
-#     stop("It must be a formula class object.")
-#   }
-#   x <- model.matrix(formula, data = data)
-#   y <- model.response(model.frame(formula, data = data))
-#   coef <- round(solve(t(x) %*% x) %*% t(x) %*% y, 5)
-#   df <- nrow(data) - length(formula)
-#   sigma_sq <- sum((y - x %*% coef)^2 / df)
-#   se <- round(diag(sqrt(sigma_sq * solve(t(x) %*% x))), 5)
-#   test_stat <- round(coef / se, 3)
-#   p_val <- round(pt(abs(test_stat), df, lower.tail = F) * 2, 5)
-#   result <- cbind(coef, se, test_stat, p_val)
-#   colnames(result) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
-#   return(as.table(result))
-# }
+#' Fitting Linear Models
+#'
+#' This function is used to fit linear models in R.
+#'
+#' @param formula a \code{formula} class object, similar to \code{lm()}.
+#' @param data input data frame.
+#' @keywords inference, perdiction
+#'
+#' @return a table with rows for each coefficient and columns for the
+#'   Estimate, Std. Error, t value, and Pr(>|t|).
+#'
+#' @examples
+#' my_lm(formula = mpg ~ hp + wt, data = mtcars))
+#'
+#' @export
+my_lm <- function(formula, data) {
+  if (class(formula) != "formula") {
+    stop("It must be a formula class object.")
+  }
+  x <- model.matrix(formula, data = data)
+  y <- model.response(model.frame(formula, data = data))
+  coef <- round(solve(t(x) %*% x) %*% t(x) %*% y, 5)
+  df <- nrow(data) - length(formula)
+  sigma_sq <- sum((y - x %*% coef)^2 / df)
+  se <- round(diag(sqrt(sigma_sq * solve(t(x) %*% x))), 5)
+  test_stat <- round(coef / se, 3)
+  p_val <- round(pt(abs(test_stat), df, lower.tail = F) * 2, 5)
+  result <- cbind(coef, se, test_stat, p_val)
+  colnames(result) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
+  return(as.table(result))
+}
