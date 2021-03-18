282eca0604e916c6a401915e112bd70b93d324ca
txqtiffany <txqtiffany@gmail.com>
2021-03-18 13:42:12 +0800

    Document and test my_knn_cv

--- R/my_knn_cv.R
+++ R/my_knn_cv.R
@@ -1,18 +1,42 @@
-# library(dplyr)
-# my_knn_cv <- function(train, cl, k_nn, k_cv) {
-#   fold <- sample(rep(1:k_cv, length = length(cl)))
-#   data <- data.frame("x" = train, "y" = cl, "split" = fold)
-#   class <- c()
-#   cv_err <- c()
-#   for (i in 1:k_cv) {
-#     data_train <- data %>% filter(split != i)
-#     data_test <- data %>% filter(split == i)
-#     perdiction <- class::knn(data_train, data_test, data_train$y, k_nn)
-#     cv_err[i] <- sum(perdiction[i] != data_train$y) / length(cl)
-#     if (cv_err[i] == mean(cv_err)) {
-#       class = class::knn(data, data, data$y, k_nn)
-#     }
-#   }
-#   output <- list("class" = class, "cv_err" = mean(cv_err))
-#   return(output)
-# }
+#' Fitting Linear Models
+#'
+#' This function is used to predict output class species using covariates bill_length_mm,
+#'  bill_depth_mm, flipper_length_mm, and body_mass_g.
+#'
+#' @param train input data frame
+#' @param cl true class value of your training data
+#' @param k_nn integer representing the number of neighbors
+#' @param k_cv integer representing the number of folds
+#' @keywords perdiction
+#'
+#' @return a list with objects
+#'
+#'  \item{class}{a vector of the predicted class Ŷ i for all observations}
+#'  \item{cv_err}{a numeric with the cross-validation misclassification error}
+#'
+#' @examples
+#' library(palmerpenguins)
+#' data(package = "palmerpenguins")
+#' penguins_df <- na.omit(penguins)
+#' train <- lapply(
+#'   penguins_df[c("bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g")],
+#'    as.numeric)
+#' cl <- as.numeric(penguins_df$species)
+#' nearest_1 <- my_knn_cv(train, cl, 1, 5)
+#' nearest_5 <- my_knn_cv(train, cl, 5, 5)
+#'
+#' @export
+my_knn_cv <- function(train, cl, k_nn, k_cv) {
+  fold <- sample(rep(1:k_cv, length = length(cl)))
+  data <- data.frame("x" = train, "y" = cl, "splits" = fold)
+  class <- c()
+  cv_err <- c()
+  for (i in 1:k_cv) {
+    data_train <- data[data$splits != i, ]
+    data_test <- data[data$splits == i, ]
+    perdiction <- class::knn(data_train, data_test, data_train$y, k_nn)
+    cv_err[i] <- sum(perdiction != data_test$y) / length(perdiction)
+  }
+  output <- list("class" = class::knn(data_train, data_test, data_train$y, k_nn), "cv_err" = mean(cv_err))
+  return(output)
+}
---
+++ tests/testthat/test-my_knn_cv.R
@@ -0,0 +1,11 @@
+penguins_df <- na.omit(my_penguins)
+train <- lapply(penguins_df[c("bill_length_mm", "bill_depth_mm",
+                              "flipper_length_mm", "body_mass_g")], as.numeric)
+cl <- as.numeric(penguins_df$species)
+nearest_1 <- my_knn_cv(train, cl, 1, 5)
+
+test_that("my_knn works", {
+  expect_type(nearest_1, "list")
+  expect_type(nearest_1$class, "integer")
+  expect_type(nearest_1$cv_err, "double")
+})
